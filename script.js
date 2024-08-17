254d44203e3aa403cc88f70e0c9faa741bb15f5c
haneefmohammed2011 <119083671+haneefmohammed2011@users.noreply.github.com>
2024-08-17 14:57:23 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+// Add any JavaScript functionality if needed
+// For now, this file can remain empty or be used to add interactivity
+console.log("JavaScript loaded. Add interactivity as needed.");
